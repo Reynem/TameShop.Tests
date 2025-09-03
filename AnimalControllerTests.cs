845cec105960d47cd91bce51a88511aa88ccb23d
Kerim_Lex <lexmeins1@gmail.com>
2025-09-03 21:35:07 +0500

    Tests for the animal controller have been added.
    Temporary folders and project builds have been excluded from the `.gitignore` file.
    Tests with database context initialization and a method for retrieving an animal have been implemented in `AnimalControllerTests.cs`.
    References to the `Microsoft.EntityFrameworkCore.InMemory` and `Microsoft.EntityFrameworkCore.Relational` packages, as well as a reference to the `TameShop` project, have been added to `TameShop.Tests.csproj`.
    Obsolete code has been removed from `Test1.cs`

---
+++ AnimalControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TameShop.Controllers;
+using TameShop.Data;
+using TameShop.Models;
+
+namespace TameShop.Tests
+{
+    [TestClass]
+    public sealed class AnimalControllerTests
+    {
+        private TameShopDbContext _context; 
+        private AnimalController _animalController;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<TameShopDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()) 
+                .Options;
+            _context = new TameShopDbContext(options); 
+            _animalController = new AnimalController(_context);
+
+            var sheep = new Animal
+            {
+                Id = 1,
+                Name = "Sheep",
+                Category = "Farm",
+                Description = "A lovely sheep",
+                ImageUrl = "https://example.com/sheep.jpg",
+                Price = 49.99M,
+            };
+
+            _context.Animals.Add(sheep);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context.Dispose();
+        }
+
+        [TestMethod]
+        public async Task AnimalGet()  
+        {
+            var response = await _animalController.GetAnimal(1);
+            Assert.IsNotNull(response);
+            Console.WriteLine(response);
+        }
+
+    }
+}
+            
